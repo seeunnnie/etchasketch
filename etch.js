028a3865780c38b317fcad60c139072055f548cb
seeunnnie <weseeuneb@gmail.com>
2023-02-24 12:40:35 -0500

    Add files via upload

--- etch.js
+++ etch.js
@@ -1,32 +1,18 @@
+
 for (let index = 0; index < 256; index++){
     const grid = document.getElementById("grid");
     const gridbox = document.createElement('div');
+    let erase = document.getElementById("erase");
     gridbox.classList.add('gridbox');
     grid.appendChild(gridbox);
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
     gridbox.addEventListener('click', function (e) {
-        this.style.backgroundColor = '#3d3d3d';
+        this.style.backgroundColor = "#" + randomColor;
     })   
     gridbox.addEventListener('dblclick', function (e){
         this.style.backgroundColor = 'white';
     })
+    erase.addEventListener('click', function (e){
+        gridbox.style.backgroundColor = 'white';
+    })
 }
-
-
-let gridSize = document.getElementById("squares")
-gridSize.addEventListener('click', function (e){
-    let gridNum = prompt("please input a grid size from 16 to 100");
-    console.log(gridNum * gridNum);
-    let newSize = gridNum * gridNum
-    for (let index = 0; index < newSize; index++){
-        const grid = document.getElementById("grid");
-        const gridbox = document.createElement('div');
-        gridbox.classList.add('gridbox');
-        grid.appendChild(gridbox);
-        gridbox.addEventListener('click', function (e) {
-            this.style.backgroundColor = '#3d3d3d';
-        })   
-        gridbox.addEventListener('dblclick', function (e){
-            this.style.backgroundColor = 'white';
-        })
-    }
-})
